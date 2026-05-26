2846ac2508459c908d378374eae5508ef8240f03
Swarmreply <nickswarmreply@gmail.com>
2026-05-26 14:11:17 -0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -13,7 +13,7 @@ const pool = new Pool({
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
   max: 20,          // maximum pool connections
   idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionTimeoutMillis: 10000,
 });
 
 // Log when pool connects
